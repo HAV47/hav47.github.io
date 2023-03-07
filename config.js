4b836889f88e366fcaf61fe29c18c44d6214aa04
HAV47 <127195494+HAV47@users.noreply.github.com>
2023-03-07 22:06:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Hải';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100079156995583';
 const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Tặng Em';
-const contentCard = 'Chúc em ngày 8-3 vui vẻ, luôn hạnh phúc và giữ nụ cười trên môi♥ ♥ ♥ ♥ From : Fan hâm mộ giấu tên';
+const contentCard = 'Chúc em ngày 8-3 vui vẻ, luôn hạnh phúc và giữ nụ cười trên môi♥ ♥ ♥ ♥ ♥ ♥ From : Fan hâm mộ giấu tên';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
